175b385bfedc0f6a9c6a53705a744c62e330ad07
Julianxalessio <julian.lombardo@outlook.com>
2025-01-24 17:27:24 +0100

    update assistant

---
+++ Assistant/commands.js
@@ -0,0 +1,14 @@
+const database = {
+  Hallo: {
+    description: "Sagt Hallo zurück.",
+    usage: "Hallo"
+  },
+  Calc: {
+    description: "Berechnt Zahlen",
+    usage: "!calc"
+  },
+  Help: {
+    description: "Zeigt diese Hilfe an.",
+    usage: "!help"
+  }
+};
