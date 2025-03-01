a5e697c891d88e28d9ec1de4ca2becc822793892
Julianxalessio <julian.lombardo@outlook.com>
2025-03-01 20:35:35 +0100

    Created Cheat Sheet

---
+++ Cheat Sheet/Database HTML.js
@@ -0,0 +1,34 @@
+const html = [
+    {
+    id: "<!DOCTYPE html>",
+    Definition: "Legt den Type des Dokuments fest."
+},
+{
+    id: "<html lang=\"de\">",
+    Definition: "Setzt die Sprache auf Deutsch."
+},
+{
+    id: "<html>",
+    Definition: "Hier kommt der ganze restliche Code rein."
+},
+{
+    id: "<head>",
+    Definition: "Platz für Code der beim starten verwändet wird wie zum Beispiel Stylesheets oder Links zu anderen Dokumenten. Hier wird auch das Logo und falls gewollt der Title festgelegt (Title geht auch im Body-Tag)."
+},
+{
+    id: "<meta>",
+    Definition: ""
+},
+{
+    id: "</head>",
+    Definition: "Closing-Tag für <head>"
+},
+{
+    id: "</html>",
+    Definition: "Closing-Tag für <html>"
+},
+{
+    id: "<>",
+    Definition: "Setzt die Sprache auf Deutsch"
+},
+];
