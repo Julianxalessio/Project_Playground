d1f28ab1f976e284eb6f01cfae3609b7c0485556
Julianxalessio <julian.lombardo@outlook.com>
2025-03-13 12:55:21 +0100

    Cleaned up

---
+++ Cheat Sheets/HTML/Attribute/DB Input.js
@@ -0,0 +1,107 @@
+const html = [
+    {
+    id: "<!DOCTYPE html>",
+    Definition: "Legt den Type des Dokuments fest."
+},
+{
+    id: "<html lang=\"de\">",
+    Definition: "Setzt die Sprache auf Deutsch."
+},
+{
+    id: "<html>",
+    Definition: "Hier kommt der ganze restliche Code rein."
+},
+{
+    id: "<head>",
+    Definition: "Platz für Code der beim starten verwändet wird wie zum Beispiel Stylesheets oder Links zu anderen Dokumenten. Hier wird auch das Logo und falls gewollt der Title festgelegt (Title geht auch im Body-Tag)."
+},
+{
+    id: "<meta>",
+    Definition: ""
+},
+{
+    id: "</head>",
+    Definition: "Closing-Tag für <head>"
+},
+{
+    id: "</html>",
+    Definition: "Closing-Tag für <html>"
+},
+{
+    id: "<link rel=\"icon\" alt=\"Logo\" href=\"Path-To-Icon.png\">",
+    Definition: "Ändert das Logo der Website (im Tab)."
+},
+{
+    id: "<link rel=\"stylesheet\" href=\"Path-To-Stylesheet.css\">",
+    Definition: "Fügt ein externes Stylesheet zur Website hinzu."
+},
+{
+    id: "<title>Name-of-the-Site</title>",
+    Definition: "Ändert den Titel der Website beliebig (im Tab)."
+},
+{
+    id: "<style></style>",
+    Definition: "Hier kommt der CSS-Code rein."
+},
+{
+    id: "<body>",
+    Definition: "Hier kommen alle Elemente wie Texte, Buttons, Divs und Bilder rein. Hier wird auch der JavaScript-Code eingefügt."
+},
+{
+    id: "</body>",
+    Definition: "Closing-Tag für <body>."
+},
+{
+    id: "<p></p>",
+    Definition: "Erstellt einen Paragraph (auf einer neuen Zeile)."
+},
+{
+    id: "<h1-6></h1-6>",
+    Definition: "Textfelder mit unterschiedlichen Grössen. 1 ist die grösste und 6 die kleinste."
+},
+{
+    id: "<div></div>",
+    Definition: "Erstellt einen Container in den Objekte eingefügt werden können. Kann auch als Textfeld dienen, wird aber grundsätzlich nie so verwändet. Es ermöglicht mehrere Objekte durch die Veränderung des Containers leicht zu ändern."
+},
+{
+    id: "<img>",
+    Definition: "Fügt ein Foto der Website hinzu."
+},
+{
+    id: "<a></a>",
+    Definition: "Dient als Textfeld, wird aber auch oft als Linktext verwändet."
+},
+{
+    id: "<>",
+    Definition: "Setzt die Sprache auf Deutsch"
+},
+{
+    id: "<>",
+    Definition: "Setzt die Sprache auf Deutsch"
+},
+{
+    id: "<>",
+    Definition: "Setzt die Sprache auf Deutsch"
+},
+{
+    id: "<>",
+    Definition: "Setzt die Sprache auf Deutsch"
+},
+{
+    id: "<>",
+    Definition: "Setzt die Sprache auf Deutsch"
+},
+{
+    id: "<>",
+    Definition: "Setzt die Sprache auf Deutsch"
+},
+{
+    id: "<>",
+    Definition: "Setzt die Sprache auf Deutsch"
+},
+{
+    id: "<>",
+    Definition: "Setzt die Sprache auf Deutsch"
+},
+
+];
